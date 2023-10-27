62f268ad1dd1c9445feb0b66cfb676e12237b857
chamomiletay <chamomile.tay@gmail.com>
2023-10-26 22:12:29 -0400

    generated header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
+//--- imports! ---
 import './App.css';
+import Header from './components/Header';
 import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       {/* Display header below*/}
-      {/* <Header /> */}
+      <Header />
 
       <h2>Currently under construction</h2>
       <p>Will be back momentarily!</p>
-
+      
     {/* Display 'About' component below */}
       <About/>
 
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+//--- imports! ---
+
+const Header = () => {
+    return(
+        <div>
+            <p>Taylor Lee | Front-End Engineer | Illustrator </p>
+        </div>
+    )
+}
+
+export default Header
